9e4099661546c8e73c6b0295916652cb5cc824be
stanley1208 <stanley.mei08@gmail.com>
2021-11-07 20:59:56 +0800

    java

---
+++ src/main/java/com/study/lambda/FunctionPractice.java
@@ -0,0 +1,27 @@
+package com.study.lambda;
+
+import java.util.function.Function;
+
+public class FunctionPractice {
+	public static void main(String[] args) {
+		Function<Integer, Integer>add2=x->x+2;
+		Function<Integer, Integer>mult3=x->x*3;
+		
+		Function<Integer, Integer>mult3add2=add2.compose(mult3);
+		Function<Integer, Integer>add2mult3=add2.andThen(mult3);
+		
+		System.out.println("mult3add2(1):"+mult3add2.apply(1));
+		System.out.println("add2mult3(2):"+add2mult3.apply(1));
+
+		Function<Integer, Integer>add2_2=x->x+2;
+		Function<String,Integer>parseThenAdd2=add2_2
+				.compose(Integer::parseInt);
+		System.out.println(parseThenAdd2.apply("1"));
+				
+		
+		Function<Integer, Integer>add2_3=x->x+2;
+		Function<Integer,String>plus2ToString=add2_3
+				.andThen(Object::toString);
+		System.out.println(plus2ToString.apply(1));
+	}
+}
