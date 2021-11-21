3827a45ae135e1cb22ffb65b2cb071aa46f95d83
stanley1208 <stanley.mei08@gmail.com>
2021-11-21 16:48:23 +0800

    java

---
+++ src/main/java/com/study/lambda/lambdaException.java
@@ -0,0 +1,50 @@
+package com.study.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+public class lambdaException {
+	public static void main(String[] args) {
+		List<Integer>values=Arrays.asList(30,10,40,10,50,90);
+//		List<Integer>scaled=lambdaException.div(values,10);
+//		System.out.println(scaled);
+		
+//		scaled=lambdaException.div(values, 0);
+//		System.out.println(scaled);
+		
+		System.out.println(divUsingMethod(values,10));
+	
+	}
+	public List<Integer>div(List<Integer>values,Integer factor){
+		return values.stream()
+				.map(n->{
+					try {
+						return n/factor;
+					} catch (ArithmeticException e) {
+						e.printStackTrace();
+					}
+					return n/factor;
+					
+				})
+				.collect(Collectors.toList());
+				
+	}
+	
+	private static Integer divide(Integer value,Integer factor) {
+		try {
+			return value/factor;
+		} catch (ArithmeticException e) {
+			e.printStackTrace();
+		}
+		return value/factor;
+	}
+	
+	public static List<Integer>divUsingMethod(List<Integer>values,Integer factor){
+		return values.stream()
+				.map(n->divide(n, factor))
+				.collect(Collectors.toList());
+	}
+}
