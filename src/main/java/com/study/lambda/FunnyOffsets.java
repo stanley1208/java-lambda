9715062fc2290f7a764dafa8fc964f8e91a318ef
stanley1208 <stanley.mei08@gmail.com>
2022-01-27 19:08:58 +0800

    java

--- src/main/java/com/study/lambda/FunnyOffsets.java
+++ src/main/java/com/study/lambda/FunnyOffsets.java
@@ -11,24 +11,20 @@ import java.util.Comparator;
 // 非整數小時的時區偏移植
 public class FunnyOffsets {
 	public static void main(String[] args) {
-		Instant instant=Instant.now();
-		ZonedDateTime current=instant.atZone(ZoneId.systemDefault());
-		System.out.printf("Current time is %s%n%n",current);
-		
-		System.out.printf("%10s %20s %13s%n","Offset","ZoneId","Time");
-		ZoneId.getAvailableZoneIds().stream()
-				.map(ZoneId::of)
-				.filter(zoneId->{
-					ZoneOffset offset=instant.atZone(zoneId).getOffset();
-					return offset.getTotalSeconds()%(60*60)!=0;
-				})
-				.sorted(Comparator.comparingInt(zoneId->
-						instant.atZone(zoneId).getOffset().getTotalSeconds()))
-				.forEach(zoneId->{
-					ZonedDateTime zdt=current.withZoneSameInstant(zoneId);
-					System.out.printf("%10s %25s %10s%n",zdt.getOffset(),zoneId,
-										zdt.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
+		Instant instant = Instant.now();
+		ZonedDateTime current = instant.atZone(ZoneId.systemDefault());
+		System.out.printf("Current time is %s%n%n", current);
+
+		System.out.printf("%10s %20s %13s%n", "Offset", "ZoneId", "Time");
+		ZoneId.getAvailableZoneIds().stream().map(ZoneId::of).filter(zoneId -> {
+			ZoneOffset offset = instant.atZone(zoneId).getOffset();
+			return offset.getTotalSeconds() % (60 * 60) != 0;
+		}).sorted(Comparator.comparingInt(zoneId -> instant.atZone(zoneId).getOffset().getTotalSeconds()))
+				.forEach(zoneId -> {
+					ZonedDateTime zdt = current.withZoneSameInstant(zoneId);
+					System.out.printf("%10s %25s %10s%n", zdt.getOffset(), zoneId,
+							zdt.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
 				});
-		
+
 	}
 }
