6a31e6360f5b7af658f8d095d4a1a972bee06775
stanley1208 <stanley.mei08@gmail.com>
2022-02-08 19:21:55 +0800

    java

--- src/main/java/com/study/lambda/ConcurrencyAndParallelism.java
+++ src/main/java/com/study/lambda/ConcurrencyAndParallelism.java
@@ -2,24 +2,54 @@ package com.study.lambda;
 
 import static org.junit.Assert.assertTrue;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+
 import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.CompilerControl.Mode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 
 public class ConcurrencyAndParallelism {
 
 	public static void main(String[] args) {
 		// 從平行切換成循序
-		List<Integer>numbers=Arrays.asList(3,1,4,1,5,9);
-		List<Integer>nums=numbers.parallelStream()
-				.map(n->n*2)
-				.peek(n->System.out.printf("%s processing %d%n",Thread.currentThread().getName(),n))
-				.sequential()
-				.sorted()
-				.collect(Collectors.toList());
+//		List<Integer>numbers=Arrays.asList(3,1,4,1,5,9);
+//		List<Integer>nums=numbers.parallelStream()
+//				.map(n->n*2)
+//				.peek(n->System.out.printf("%s processing %d%n",Thread.currentThread().getName(),n))
+//				.sequential()
+//				.sorted()
+//				.collect(Collectors.toList());
+		
+//		Instant before=Instant.now();
+//		int total=IntStream.of(3,1,4,1,5,9)
+//				.map(ConcurrencyAndParallelism::doubleIt)
+//				.sum();
+		// 使用平行串流
+//		int total=IntStream.of(3,1,4,1,5,9)
+//				.parallel()
+//				.map(ConcurrencyAndParallelism::doubleIt)
+//				.sum();
+//		
+//		Instant after=Instant.now();
+//		Duration duration=Duration.between(before, after);
+//		System.out.println("Total of doubles = "+total);
+//		System.out.println("time = "+duration.toMillis()+"ms");
+		
+		
+		
+		
 	}
 
 	// 建立循序串流
@@ -66,6 +96,14 @@ public class ConcurrencyAndParallelism {
 //		assertTrue(numbers.parallelStream().sequential().isParallel());
 //	}
 
-	
+	// 對循序串流加上整數
+	public static int doubleIt(int n) {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException ignore) {
+			// TODO: handle exception
+		}
+		return n*2;
+	}
 			
 }
---
+++ src/main/java/com/study/lambda/DoublingDemo.java
@@ -0,0 +1,89 @@
+package com.study.lambda;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import org.openjdk.jmh.annotations.*;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+@Fork(value = 2,jvmArgs = {"-Xms4G","-Xmx4G"})
+public class DoublingDemo {
+
+	public static long N=Long.MAX_VALUE;
+	public int doubleIt(int n) {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			
+		}
+		return n*2;
+	}
+	
+	@Benchmark
+	public int doubleAndSumSequential() {
+		return IntStream.of(3,1,4,1,5,9)
+				.map(this::doubleIt)
+				.sum();
+	}
+	
+	@Benchmark
+	public int doubleAndSumParallel() {
+		return IntStream.of(3,1,4,1,5,9)
+				.parallel()
+				.map(this::doubleIt)
+				.sum();
+	}
+	
+	// 在迴圈中迭代家總數字
+	public long iterativeSum() {
+		long result=0;
+		
+		for(long i=1L;i<=N;i++) {
+			result+=i;
+		}
+		return result;
+	}
+	
+	// 總和泛型串流
+	public long sequentialStreamSum() {
+		return Stream.iterate(1L, i->i+1)
+				.limit(N)
+				.reduce(0L, Long::sum);
+				
+				
+	}
+	
+	public long parallelStreamSum() {
+		return Stream.iterate(1L, i->i+1)
+				.limit(N)
+				.parallel()
+				.reduce(0L, Long::sum);
+				
+				
+	}
+	
+	// 使用LongStream
+	public long sequentialLongStreamSum() {
+		return LongStream.rangeClosed(1, N)
+				.sum();
+	}
+	
+	public long parallelLongStreamSum() {
+		return LongStream.rangeClosed(1, N)
+				.parallel()
+				.sum();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		org.openjdk.jmh.Main.main(args);
+
+	}
+
+}
